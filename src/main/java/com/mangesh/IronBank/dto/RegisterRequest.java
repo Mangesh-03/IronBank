a6a2dbf680ff2c341bfc07277de0949db4c5dfd7
Mangesh03 <mangeshbedre02@gmail.com>
2026-06-15 11:31:01 +0530

    Added DTOs for register and login

---
+++ src/main/java/com/mangesh/IronBank/dto/RegisterRequest.java
@@ -0,0 +1,26 @@
+package com.mangesh.IronBank.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+}
---
+++ src/main/java/com/mangesh/IronBank/dto/RegisterResponse.java
@@ -0,0 +1,18 @@
+package com.mangesh.IronBank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponse
+{
+    private Long id;
+    private String fullName;
+    private String email;
+    private String message;  // "User registered successfully"
+}
