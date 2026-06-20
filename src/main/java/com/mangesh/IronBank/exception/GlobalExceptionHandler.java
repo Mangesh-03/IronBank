faebae68710ba57c2c2dc569bba62a6690a33be5
Mangesh03 <mangeshbedre02@gmail.com>
2026-06-21 01:57:20 +0530

    Added logging to GlobalExceptionHandler for debugging

--- src/main/java/com/mangesh/IronBank/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mangesh/IronBank/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
@@ -13,6 +15,8 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class GlobalExceptionHandler
 {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex)
     {
@@ -115,6 +119,8 @@ public class GlobalExceptionHandler
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneral(Exception ex)
     {
+        log.error("Unhandled exception occurred", ex);
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.builder()
