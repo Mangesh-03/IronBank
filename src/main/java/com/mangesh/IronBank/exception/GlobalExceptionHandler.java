41f89829d997e130dfc8aa27ba0a104b12b6df68
Mangesh03 <mangeshbedre02@gmail.com>
2026-06-21 12:43:16 +0530

    Revert "Added logging to GlobalExceptionHandler for debugging"
    This reverts commit faebae68710ba57c2c2dc569bba62a6690a33be5.

--- src/main/java/com/mangesh/IronBank/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mangesh/IronBank/exception/GlobalExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
@@ -15,8 +13,6 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class GlobalExceptionHandler
 {
-    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex)
     {
@@ -119,8 +115,6 @@ public class GlobalExceptionHandler
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneral(Exception ex)
     {
-        log.error("Unhandled exception occurred", ex);
-
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.builder()
